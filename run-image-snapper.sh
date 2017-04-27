a8d3c7f0c024bdc743463d85e4de5234b88a193c
janchristoph <mail@janchristoph.de>
2017-04-27 13:13:17 +0200

    cleanup

--- run-image-snapper.sh
+++ run-image-snapper.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#watch -n5 "fswebcam -r 800x600 --jpeg 85 -S 10 image.jpg"
-
 for (( ; ; ))
 do
 	ffmpeg -y -i http://localhost:8080/ -ss 00:00:05.000 -f image2 -vframes 1 image.jpg
--- run-streamer.sh
+++ run-streamer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 
-#vlc -I dummy v4l2:///dev/video0 --sout "#transcode{vcodec=theo,vb=200,fps=10,scale=Automatisch,width=640,height=480,acodec=none}:http{mux=webm,dst=:8080/}"
 vlc -I dummy v4l2:///dev/video0 --sout "#transcode{vcodec=theo,vb=200,fps=15,width=640,height=480,acodec=none}:http{mux=ogg,dst=:8080/}"
 
